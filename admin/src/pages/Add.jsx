77087117f739eab958dfe60cdda68bc0718d92b8
ksanthoshui <k.santhosh.ui@gmail.com>
2026-07-16 14:29:00 +0530

    dev add product

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,11 +1,291 @@
-import React from 'react'
+import React, { useState } from "react";
+import { CloudUpload } from "lucide-react";
+import axios from 'axios'
+import {backend_url} from '../App'
+import { toast } from "react-toastify";
+function Add({token}) {
+  const [image1, setimage1] = useState(false);
+  const [image2, setimage2] = useState(false);
+  const [image3, setimage3] = useState(false);
+  const [image4, setimage4] = useState(false);
 
-function Add() {
-  return (
-    <>
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("Men");
+  const [subCategory, setSubCategory] = useState("Topwear");
+  const [bestseller, setBestseller] = useState(false);
+  const [sizes, setSizes] = useState([]);
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const formData = new FormData();
+
+      formData.append("name", name);
+      formData.append("description", description);
+      formData.append("price", price);
+      formData.append("category", category);
+      formData.append("subCategory", subCategory);
+      formData.append("bestseller", bestseller);
+      formData.append("sizes", JSON.stringify(sizes));
+
+      image1 && formData.append("image1", image1);
+      image2 && formData.append("image2", image2);
+      image3 && formData.append("image3", image3);
+      image4 && formData.append("image4", image4);
       
-    </>
-  )
+      const response = await axios.post(backend_url + '/api/product/add',formData,{headers:{token}})
+      if (response.data.success) {
+    toast.success(response.data.message)
+    setName('')
+    setDescription('')
+    setimage1(false)
+    setimage2(false)
+    setimage3(false)
+    setimage4(false)
+    setPrice('')
+} else {
+    toast.error(response.data.message)
 }
 
-export default Add
+    } catch (error) {
+      console.log(error);
+    toast.error(error.message)
+    }
+  };
+  return (
+    <>
+      <form onSubmit={onSubmitHandler} className=" flex flex-col w-full items-start">
+        <div>
+          <p className="mb-2 capitalize">upload Image</p>
+          <div className="flex gap-2 h-15 ">
+            <label htmlFor="image1" className="">
+              {!image1 ? (
+                <CloudUpload className=" p-2 w-15 h-full border border-dotted border-gray-400 " />
+              ) : (
+                <img
+                  src={URL.createObjectURL(image1)}
+                  className="  w-15 h-full  "
+                  alt="not found"
+                />
+              )}
+              <input
+                type="file"
+                id="image1"
+                hidden
+                onChange={(e) => setimage1(e.target.files[0])}
+              />
+            </label>
+            <label htmlFor="image2" className="">
+              {!image2 ? (
+                <CloudUpload className=" p-2 w-15 h-full border border-dotted border-gray-400 " />
+              ) : (
+                <img
+                  src={URL.createObjectURL(image2)}
+                  className=" w-15 h-full  "
+                  alt="not found"
+                />
+              )}
+              <input
+                type="file"
+                id="image2"
+                onChange={(e) => setimage2(e.target.files[0])}
+                hidden
+              />
+            </label>
+            <label htmlFor="image3" className="">
+              {!image3 ? (
+                <CloudUpload className=" p-2 w-15 h-full border border-dotted border-gray-400 " />
+              ) : (
+                <img
+                  src={URL.createObjectURL(image3)}
+                  className=" w-15 h-full  "
+                  alt="not found"
+                />
+              )}
+              <input
+                type="file"
+                id="image3"
+                onChange={(e) => setimage3(e.target.files[0])}
+                hidden
+              />
+            </label>
+            <label htmlFor="image4" className="">
+              {!image4 ? (
+                <CloudUpload className=" p-2 w-15 h-full border border-dotted border-gray-400 " />
+              ) : (
+                <img
+                  src={URL.createObjectURL(image4)}
+                  className="  w-15 h-full  "
+                  alt="not found"
+                />
+              )}
+              <input
+                type="file"
+                id="image4"
+                onChange={(e) => setimage4(e.target.files[0])}
+                hidden
+              />
+            </label>
+          </div>
+        </div>
+        <div className="w-full">
+          <p className="mb-2">Product Name</p>
+          <input
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            className="w-full max-w-100 px-3 py-2"
+            type="text"
+            placeholder="Type Here"
+            required
+          />
+        </div>
+        <div className="w-full">
+          <p className="mb-2">Product description</p>
+          <textarea
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+            className="w-full max-w-100 px-3 py-2"
+            type="text"
+            placeholder="Write Content Here"
+            required
+          />
+        </div>
+
+        <div className="flex flex-col sm:flex-row gap-2 w-full sm:gap-6 ">
+          <div>
+            <p className="mb-2">Product Category</p>
+            <select
+              className="w-full px-3 py-2"
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option value="Men">Men</option>
+              <option value="Women">Women</option>
+              <option value="Kids">Kids</option>
+            </select>
+          </div>
+          <div>
+            <p className="mb-2">Sub category</p>
+            <select
+              className="w-full px-3 py-2"
+              onChange={(e) => setSubCategory(e.target.value)}
+            >
+              <option value="Topwear">Topwear</option>
+              <option value="Bottomwear">Bottomwear</option>
+              <option value="Winterwear">Winterwear</option>
+            </select>
+          </div>
+          <div>
+            <p className="mb-2">Product Price</p>
+            <input
+              type="number"
+              onChange={(e) => setPrice(e.target.value)}
+              value={price}
+              placeholder="25"
+              className="w-full px-3 py-2 sm:w-30"
+            />
+          </div>
+        </div>
+        <div>
+          <p className="mb-2">Product Size</p>
+          <div className="flex gap-3">
+            <div
+              onClick={() =>
+                setSizes((prev) =>
+                  prev.includes("S")
+                    ? prev.filter((item) => item !== "S")
+                    : [...prev, "S"],
+                )
+              }
+            >
+              <p
+                className={`${sizes.includes("S") ? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}
+              >
+                S
+              </p>
+            </div>
+            <div
+              onClick={() =>
+                setSizes((prev) =>
+                  prev.includes("M")
+                    ? prev.filter((item) => item !== "M")
+                    : [...prev, "M"],
+                )
+              }
+            >
+              <p
+                className={`${sizes.includes("M") ? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}
+              >
+                M
+              </p>
+            </div>
+            <div
+              onClick={() =>
+                setSizes((prev) =>
+                  prev.includes("L")
+                    ? prev.filter((item) => item !== "L")
+                    : [...prev, "L"],
+                )
+              }
+            >
+              <p
+                className={`${sizes.includes("L") ? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}
+              >
+                L
+              </p>
+            </div>
+            <div
+              onClick={() =>
+                setSizes((prev) =>
+                  prev.includes("XL")
+                    ? prev.filter((item) => item !== "XL")
+                    : [...prev, "XL"],
+                )
+              }
+            >
+              <p
+                className={`${sizes.includes("XL") ? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}
+              >
+                XL
+              </p>
+            </div>
+            <div
+              onClick={() =>
+                setSizes((prev) =>
+                  prev.includes("XXL")
+                    ? prev.filter((item) => item !== "XXL")
+                    : [...prev, "XXL"],
+                )
+              }
+            >
+              <p
+                className={`${sizes.includes("XXL") ? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}
+              >
+                XXL
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex gap-2 mt-2">
+          <input
+            type="checkbox"
+            id="bestseller"
+            onChange={() => setBestseller((prev) => !prev)}
+            checked={bestseller}
+          />
+          <label htmlFor="bestseller" className="cursor-pointer">
+            Add to bestseller{" "}
+          </label>
+        </div>
+
+        <button className="w-28 py-3 mt-4 bg-black text-white " type="submit">
+          ADD
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default Add;
